a3d4f452e5595717ef45e87c2def88830493c51e
bohdanImagex <bohdan@imagexmedia.com>
2022-08-07 19:08:57 +0300

    GET parameters.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
   const API_URL = 'https://reqres.in/api/users?';
   const totalElement = document.getElementById('total');
   const pageNumberElement = document.getElementById('page-number');
+  const urlParams = new URLSearchParams(window.location.search);
   const btnNext = document.getElementById('btn_next');
   const btnPrev = document.getElementById('btn_prev');
   const BUTTON_TYPES = {
@@ -11,36 +12,40 @@
   const pagination = {
     page: 1,
     perPage: 2,
-    totalCount: 0,
+    totalCount: 12,
     pageNumber: 1,
   }
+  let currentPage = urlParams.get('current_page') ?? pagination.page;
+  let perPage = urlParams.get('per_page') ?? pagination.perPage;
 
 // Send request and work with data.
-async function load() {
-  const fetchURL = API_URL + new URLSearchParams({page: pagination.page, per_page: pagination.perPage,});
-  const response = await fetch(fetchURL);
-  const responseData = await response.json();
-  const { total, data } = responseData;
-  // Render total results.
-  totalElement.innerHTML = `${pagination.perPage} of ${responseData.total} Records`;
+  async function load() {
+    const fetchURL = API_URL + new URLSearchParams({page: currentPage, per_page: perPage,});
+    const response = await fetch(fetchURL);
+    const responseData = await response.json();
+    const { total, data } = responseData;
+    pagination.totalCount = total;
+    // Render total results.
+    totalElement.innerHTML = `${perPage} of ${responseData.total} Records`;
 
-  const result = document.querySelector('#result');
-  // Clear result.
-  result.innerHTML = ""
+    const result = document.querySelector('#result');
+    // Clear result.
+    result.innerHTML = ""
 
-  // separate function for getting the HTML.
-  const createUsersHTMLFromData = (usersData) => `<div class="user-info"><div class="user-first-name">${usersData.first_name}</div><div class="user-last-name">${usersData.last_name}</div><div class="user-email">${usersData.email}</div><div class="user-avatar"><img src="${usersData.avatar}"></div></div>`;
+    // separate function for getting the HTML.
+    const createUsersHTMLFromData = (usersData) => `<div class="user-info"><div class="user-first-name">${usersData.first_name}</div><div class="user-last-name">${usersData.last_name}</div><div class="user-email">${usersData.email}</div><div class="user-avatar"><img src="${usersData.avatar}"></div></div>`;
 
-  const html = data.map(function(usersData) {
-    // here we need to generate html for object.
-    return createUsersHTMLFromData(usersData);
-  })
+    const html = data.map(function(usersData) {
+      // here we need to generate html for object.
+      return createUsersHTMLFromData(usersData);
+    })
 
-  result.insertAdjacentHTML('afterbegin', html.join(''))
+    result.insertAdjacentHTML('afterbegin', html.join(''))
 }
 
+// оце не робить курва.
   function numPages() {
-    return Math.ceil(pagination.totalCount / pagination.perPage);
+    return Math.ceil(pagination.totalCount / perPage);
   }
 
   // Set number of page.
@@ -48,38 +53,47 @@ async function load() {
     pageNumberElement.innerHTML = pageNumber;
 
     // Hide Prev button if we are on the first page.
-    btnPrev.style.visibility = pageNumber === 1 ? 'hidden' : 'visible';
+    btnPrev.style.visibility = pageNumber == 1 ? 'hidden' : 'visible';
 
-    // Hide Next button if we are on the last page.
-    btnNext.style.visibility = pageNumber === numPages() ? 'hidden' : 'visible';
+    btnNext.style.visibility = pageNumber == numPages() ? 'hidden' : 'visible';
   }
 
   function setPageLoad() {
-    setPageNumber(pagination.page);
     load();
+    setPageNumber(currentPage);
   }
 
   function handlePaginationButtonClick(btnType) {
-    if (BUTTON_TYPES.NEXT === 'next') {
-      pagination.page++;
+    if (btnType === BUTTON_TYPES.NEXT) {
+      currentPage++
       setPageLoad();
+      // Set new GET parameters.
+      urlParams.set('current_page', currentPage);
+      urlParams.set('per_page', perPage);
+      sendData(urlParams)
     }
-    else if (BUTTON_TYPES.PREV === 'prev') {
-      pagination.page--;
+    else if (btnType === BUTTON_TYPES.PREV) {
+      currentPage--;
       setPageLoad();
+      // Set new GET parameters.
+      urlParams.set('current_page', currentPage);
+      urlParams.set('per_page', perPage);
+      sendData(urlParams)
     }
   }
 
 // Load first result and set page number when page is loaded.
   document.addEventListener('DOMContentLoaded', function () {
     load();
-    setPageNumber(pagination.page);
+    setPageNumber(currentPage);
   })
 
 
 // Get perPage value from the select.
   document.getElementById('page-size').addEventListener('change', function() {
-    pagination.perPage = this.value
+    perPage = this.value
+    urlParams.set('per_page', perPage);
+    sendData(urlParams)
     load()
   });
 
@@ -88,6 +102,13 @@ async function load() {
     event.preventDefault();
     handlePaginationButtonClick('next')
   });
+
+
+  function sendData(urlParams) {
+    // Replace get parameters in browser.
+    window.history.replaceState(null, null, `?${urlParams.toString()}`)
+  }
+
 
 // Click on the Prev button.
   btnPrev.addEventListener('click', function (event) {
